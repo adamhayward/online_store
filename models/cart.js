555d4d73fdfbd58ec92f896828a064fba2d832c7
Adam Hayward <a.hayward11@gmail.com>
2021-01-20 14:06:12 -0900

    creaed model for cart sql table

---
+++ models/cart.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define("Cart", {
+         // proceeding to define cols:
+         total: DataTypes. NUM,
+         //TODO ensure data is formated with $ & .
+    });
+    return Cart;
+}
