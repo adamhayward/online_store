64d57c10b36b341d1ecf38c24f8737b400665b0e
Adam Hayward <a.hayward11@gmail.com>
2021-01-28 12:26:58 -0900

    initial heroku commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,7 @@
 $(document).ready(() => {
   const mainContent = $("#cards");
 
-  const button = $("#addCart1");
-  button.on("click", () => createItemCard());
+  $(document).on("click", "button", handleSelectItem);
 
   getInventory();
 
@@ -11,7 +10,7 @@ $(document).ready(() => {
     let newImg = $("<img>").addClass("card-img-top");
     let newH5 = $("<h5>").addClass("card-title");
     let newPEl = $("<p>").addClass("card-text");
-    let newAEl = $("<a>");
+    let newLable = $("<lable>").addClass("mr-4");
     let newBttn = $("<button>").addClass("btn");
     // building new card element
     let img = newImg.attr("src", itemData.imageURL);
@@ -21,16 +20,18 @@ $(document).ready(() => {
       .append(
         newH5.append(itemData.item),
         newPEl.append(itemData.description),
-        newAEl.append(itemData.price),
+        newLable.append(`$${itemData.price}`),
         newBttn.append("Add To Cart")
       );
+
     let newCard = $("<div>")
       .addClass("card")
       .css("width", "18rem")
-      .append(img, newCardData);
+      .append(img, newCardData.data("item", itemData));
 
-    let newCol = $("<div>").addClass("col-lg-4 col-md-6 card-col")
-    .append(newCard);
+    let newCol = $("<div>")
+      .addClass("col-lg-4 col-md-6 card-col mt-3")
+      .append(newCard);
     // getInventory(newCard);
     mainContent.append(newCol);
   }
@@ -40,21 +41,37 @@ $(document).ready(() => {
     $.get("/api/inventory", (data) => {
       let cardToAdd = [];
       for (let i = 0; i < data.length; i++) {
-        cardToAdd.push(createItemCard(data[i]));
-        console.log(data);
-        console.log(cardToAdd);
+        cardToAdd.push(data[i]);
+        // cardToAdd.push(createItemCard(data[i]));
+        createItemCard(data[i]);
+        console.log(data[i]);
       }
       renderItemCard(cardToAdd);
       console.log(cardToAdd);
     });
   }
-
+  /* function telling brouser to render a new card weather or not there is any items in 
+    inventory table*/
   function renderItemCard(card) {
     if (card.length) {
-      console.log(card);
       mainContent.append(card);
     } else {
-      console.log("nope");
+      alert("Sorry there are not any apps for sale at this time");
     }
+  }
+
+  function handleSelectItem() {
+    let itemData = $(this).parent("div").data("item");
+    let id = itemData.id;
+    $.ajax({
+      method: "get",
+      url: `/api/cart/${id}`,
+    })
+    // .then(
+    //   $.ajax({
+    //     method: "post",
+    //     url: `/api/cart/${id}`,
+    //   })
+    // );
   }
 });
